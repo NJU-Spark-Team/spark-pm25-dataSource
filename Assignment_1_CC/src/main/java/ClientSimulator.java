3bc3ee16e1a0dc03ed1d10a2e1ecb192fe4e4b1d
Disclover <727296166@qq.com>
2019-10-12 20:58:26 +0800

    add logicClient

---
+++ Assignment_1_CC/src/main/java/ClientSimulator.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.net.Socket;
+
+public class ClientSimulator {
+    private static final String host = "127.0.0.1";
+    private static final int port = 9999;
+
+    public static void main(String[] args) {
+        Socket socket = null;
+        try {
+            socket = new Socket(host, port);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            writer.write("\n");
+            writer.flush();
+            String line = reader.readLine();
+            System.out.println(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ Assignment_1_CC/src/main/java/LogicServer.java
@@ -0,0 +1,74 @@
+import CSV.MongoUtil;
+import org.bson.Document;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class LogicServer {
+    public static MongoUtil mongoUtil = new MongoUtil("SparkStreaming");
+    public static void main(String[] args) {
+        new LogicServer().startAction();
+    }
+    private void startAction() {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(9999);
+            System.out.println("Mission Ready!");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new Thread(new socketServerThread(socket)).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    class socketServerThread implements Runnable{
+        Socket socket;
+        BufferedReader reader;
+        BufferedWriter writer;
+
+        public void run() {
+            try {
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+                while (reader.readLine() != null){
+                    Document document = LogicServer.mongoUtil.selectNext("Beijing");
+                    writer.write(document.toJson() + "\n");
+                    writer.flush();
+                }
+            } catch (Exception e){
+    //            e.printStackTrace();
+            } finally {
+                try {
+                    if (reader != null){
+                        reader.close();
+                    }
+                    if (writer != null){
+                        writer.close();
+                    }
+                    if (socket != null){
+                        socket.close();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public socketServerThread(Socket socket){
+            super();
+            this.socket = socket;
+        }
+    }
+}
+
